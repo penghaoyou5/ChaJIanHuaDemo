e8f3e683018d209394247f365ceee9dfc617a3ff
Your Name <1301333734@qq.com>
2016-04-18 23:54:05 +0800

    正常不加反射插件化代码运行正常

--- DL2Host/dl2client/src/main/java/dl2host/dl2client/MainActivity.java
+++ DL2Host/dl2client/src/main/java/dl2host/dl2client/MainActivity.java
@@ -1,9 +1,8 @@
 package dl2host.dl2client;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
 
     //
